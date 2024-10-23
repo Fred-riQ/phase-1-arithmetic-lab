fc2bb601570981be0a268d89ca7536b2e63c00d9
Fred-riQ <fredrickbet5@gmail.com>
2024-10-23 19:16:26 +0300

    added arithmetc lab2

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1*num2;
+
+let random = Math.floor(Math.random()*5) +1;
+   
+
+let num3 = 25;
+let num4 = 7;
+let mod = num3%num4;
+
+
+let numbers = [4,8,16,20,2];
+let max = Math.max(...numbers);
